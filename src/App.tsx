a181ab1c900a40a2489757e2235852b07a78f7ca
Alan Escobedo <alaanprogramacion@gmail.com>
2022-03-24 10:12:39 -0600

    Add MainCard element

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,14 @@ function App() {
 
   return (
     <div>
-      ʕ•́ᴥ•̀ʔっayo
-      ʕ•́ᴥ•̀ʔっayo!
+      <div style={{ width:"90%", margin:"10% auto" }}>
+        <div style={{ height: "100vh" }}>
+          <div style={{ height: "55px", backgroundColor: "#CEB4F91a", border: "2px solid #CEB4F9", borderRadius: "10px", padding: "10px", display: "grid", placeContent: "center", gridTemplateColumns: "1fr 1fr", justifyItems: "center", alignContent:"center", color:"#6A6A6A" }}>
+            <h4>Username</h4>
+            <h4>20-04-2022</h4>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
