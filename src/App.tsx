371819a37b16a3582f86fdb1c1a09acd53dcd98e
Alan Escobedo <alaanprogramacion@gmail.com>
2022-03-24 10:29:42 -0600

    Refactor MainCard in a component, and extract margin and width properties

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
+import { MainCard } from "./components/MainCard"
+
 function App() {
 
   return (
-    <div>
-      <div style={{ width: "90%", margin: "10% auto" }}>
-        <div style={{ height: "55px", backgroundColor: "#CEB4F91a", border: "2px solid #CEB4F9", borderRadius: "10px", padding: "10px", display: "grid", placeContent: "center", gridTemplateColumns: "1fr 1fr", justifyItems: "center", alignContent: "center", color: "#6A6A6A" }}>
-          <h4>Username</h4>
-          <h4>20-04-2022</h4>
-        </div>
-      </div>
+    <div style={{width:"90%", margin: "10% auto" }}>
+      <MainCard />
     </div>
   )
 }
---
+++ src/components/MainCard.tsx
@@ -0,0 +1,10 @@
+export const MainCard = () => {
+  return (
+    <div>
+      <div style={{ height: "55px", backgroundColor: "#CEB4F91a", border: "2px solid #CEB4F9", borderRadius: "10px", padding: "10px", display: "grid", placeContent: "center", gridTemplateColumns: "1fr 1fr", justifyItems: "center", alignContent: "center", color: "#6A6A6A" }}>
+        <h4>Username</h4>
+        <h4>20-04-2022</h4>
+      </div>
+    </div>
+  )
+}
