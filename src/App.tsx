929da79c276dcc55a5f6d98fdb47303508eeea53
Alan Escobedo <alaanprogramacion@gmail.com>
2022-03-24 10:24:34 -0600

    Remove unnecessary HTML element

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,10 @@ function App() {
 
   return (
     <div>
-      <div style={{ width:"90%", margin:"10% auto" }}>
-        <div style={{ height: "100vh" }}>
-          <div style={{ height: "55px", backgroundColor: "#CEB4F91a", border: "2px solid #CEB4F9", borderRadius: "10px", padding: "10px", display: "grid", placeContent: "center", gridTemplateColumns: "1fr 1fr", justifyItems: "center", alignContent:"center", color:"#6A6A6A" }}>
-            <h4>Username</h4>
-            <h4>20-04-2022</h4>
-          </div>
+      <div style={{ width: "90%", margin: "10% auto" }}>
+        <div style={{ height: "55px", backgroundColor: "#CEB4F91a", border: "2px solid #CEB4F9", borderRadius: "10px", padding: "10px", display: "grid", placeContent: "center", gridTemplateColumns: "1fr 1fr", justifyItems: "center", alignContent: "center", color: "#6A6A6A" }}>
+          <h4>Username</h4>
+          <h4>20-04-2022</h4>
         </div>
       </div>
     </div>
