253b9513a114772ff03fc45b6e2c86ca5c9aa7c9
Alan Escobedo <alanescobedo.dev@gmail.com>
2022-03-25 17:19:59 -0600

    Remove ping/pong

--- src/config/app.ts
+++ src/config/app.ts
@@ -4,10 +4,6 @@ import taskRouter from '@task/taskRouter'
 const app = express()
 app.use(express.json())
 
-app.get("/ping", (_req, res) => {
-  res.send("pong")
-})
-
 app.use('/api/v1/tasks', taskRouter)
 
 export default app
