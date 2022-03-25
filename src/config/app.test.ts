13b36c35c4ff225a0f5c387dfb99ad5d889526f9
Alan Escobedo <alanescobedo.dev@gmail.com>
2022-03-25 12:15:38 -0600

    Add initial test to create new task

--- src/config/app.test.ts
+++ src/config/app.test.ts
@@ -2,9 +2,21 @@ import request from 'supertest'
 import app from './app'
 
 describe('Get /ping', () => {
-
   it('should return pong', async () => {
     const response = await request(app).get('/ping').send()
     expect(response.text).toBe('pong')
+  })
+})
+
+describe("POST /api/v1/task", () => {
+  const newTask = {
+    title: "Test title",
+    status: "pending",
+    scheduledFor: "2022-06-06T00:00:00.000Z"
+  }
+  test("should return a new task created", async () => {
+    const response = await request(app).post("/api/v1/task").send(newTask)
+    expect(response.status).toBe(201)
+    expect(response.body).toEqual(newTask)
   })
 })
--- src/config/app.ts
+++ src/config/app.ts
@@ -1,9 +1,18 @@
 import express from 'express'
 
 const app = express()
+app.use(express.json())
 
 app.get("/ping", (_req, res) => {
   res.send("pong")
+})
+
+app.post("/api/v1/task", async (req, res) => {
+  const { title, status, scheduledFor } = req.body
+
+  const taskCreated = { title, status, scheduledFor }
+
+  return res.status(201).send(taskCreated)
 })
 
 export default app
