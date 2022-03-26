7d143b8d135b9cab5c725b49314f7b0ee8d73e25
Alan Escobedo <alanescobedo.dev@gmail.com>
2022-03-26 17:44:54 -0600

    Changes in tests files

--- src/seed/seed.test.ts
+++ src/seed/seed.test.ts
@@ -2,31 +2,36 @@ import request from 'supertest'
 import app from '@config/app'
 import { seedTasks } from '@seed/seed-tasks'
 
-describe('Test Seeds /api/v1/seeds/task', () => {
-  beforeAll(async () => {
-    await request(app).get('/api/v1/seeds/flush')
+describe('SEED.TEST.TS -- TASK /api/v1/seeds/task', () => {
+  describe('Generate Seeds tasks', () => {
+    beforeEach(async () => {
+      await request(app).get('/api/v1/seeds/flush')
+    })
+    test('should return status 200 and message "Seed DB created"', async () => {
+      const response = await request(app).get('/api/v1/seeds/generate')
+      expect(response.status).toBe(200)
+      expect(response.body).toHaveProperty('message', 'Seed DB created')
+    })
+    test('should add the initial data to the database', async () => {
+      await request(app).get('/api/v1/seeds/generate')
+
+      await request(app).get('/api/v1/tasks/create-index')
+      const { body: tasks } = await request(app).get('/api/v1/tasks/search')
+      expect(tasks).toHaveLength(seedTasks.tasks.length)
+    })
   })
+  describe('Flush database', () => {
+    test('should return status 200 and message "Data flushed"', async () => {
+      const response = await request(app).get('/api/v1/seeds/flush')
+      expect(response.status).toBe(200)
+      expect(response.body).toHaveProperty('message', 'DB flushed')
+    })
+    test('should empty the database', async () => {
+      await request(app).get('/api/v1/tasks/create-index')
 
-  test('should return status 200 and message "Seed DB created"', async () => {
-    const response = await request(app).get('/api/v1/seeds/generate')
-    expect(response.status).toBe(200)
-    expect(response.body).toHaveProperty('message', 'Seed DB created')
-
-    await request(app).get('/api/v1/tasks/create-index')
-
-    const { body: tasks } = await request(app).get('/api/v1/tasks/search')
-    expect(tasks).toHaveLength(seedTasks.tasks.length)
-  })
-
-  test('should return status 200 and message "Data flushed"', async () => {
-    const response = await request(app).get('/api/v1/seeds/flush')
-    expect(response.status).toBe(200)
-    expect(response.body).toHaveProperty('message', 'DB flushed')
-
-    await request(app).get('/api/v1/tasks/create-index')
-
-    const { body: tasks } = await request(app).get('/api/v1/tasks/search')
-    expect(tasks).toBeInstanceOf(Array)
-    expect(tasks).toHaveLength(0)
+      const { body: tasks } = await request(app).get('/api/v1/tasks/search')
+      expect(tasks).toBeInstanceOf(Array)
+      expect(tasks).toHaveLength(0)
+    })
   })
 })
--- src/task/task.test.ts
+++ src/task/task.test.ts
@@ -1,5 +1,6 @@
 import request from 'supertest'
 import app from '@config/app'
+import { seedTasks } from '@seed/seed-tasks'
 
 const newTask = {
   title: 'Test title',
@@ -8,62 +9,61 @@ const newTask = {
 }
 const fields = Object.entries(newTask)
 
-describe('GET /api/v1/tasks/createindex', () => {
-  test('should return status 200 and message Index created', async () => {
-    const response = await request(app).get('/api/v1/tasks/create-index')
-    expect(response.status).toBe(200)
-    expect(response.body).toHaveProperty('message', 'Index created')
+describe('TASK TESTS /api/v1/tasks', () => {
+  describe('GET /createindex', () => {
+    test('should return status 200 and message Index created', async () => {
+      const response = await request(app).get('/api/v1/tasks/create-index')
+      expect(response.status).toBe(200)
+      expect(response.body).toHaveProperty('message', 'Index created')
+    })
   })
-})
+  describe('CRUD Operations', () => {
+    beforeEach(async () => {
+      await request(app).get('/api/v1/seeds/flush')
+      await request(app).get('/api/v1/tasks/create-index')
+    })
+    describe('POST /api/v1/tasks', () => {
+      test('should return a new task created', async () => {
+        const response = await request(app).post('/api/v1/tasks').send(newTask)
+        expect(response.status).toBe(201)
+        expect(response.body).toHaveProperty('entityId')
+        for (const [key, value] of fields) {
+          expect(response.body).toHaveProperty(key, value)
+        }
+      })
+    })
+    describe('GET /api/v1/tasks', () => {
+      test('should return all tasks', async () => {
+        await request(app).post('/api/v1/tasks').send(newTask)
 
-describe('CRUD operations', () => {
-  beforeEach(async () => {
-    await request(app).get('/api/v1/seeds/flush')
-    await request(app).get('/api/v1/tasks/create-index')
+        const response = await request(app).get('/api/v1/tasks/search')
+        expect(response.status).toBe(200)
+        expect(response.body).toBeInstanceOf(Array)
+      })
+      test('should return all the initial data', async () => {
+        await request(app).get('/api/v1/seeds/generate')
+        const response = await request(app).get('/api/v1/tasks/search')
+
+        expect(response.body.length).toBe(seedTasks.tasks.length)
+      })
+    })
+    describe('DELETE /api/v1/tasks/:id', () => {
+      test('should return status 200 and message Task deleted', async () => {
+        const task = await request(app).post('/api/v1/tasks').send(newTask)
+        const { entityId } = task.body
+
+        const response = await request(app).delete(`/api/v1/tasks/${entityId as string}`)
+        expect(response.status).toBe(200)
+        expect(response.body).toHaveProperty('message', 'Task deleted')
+      })
+      test('should return error 400 if id is wrong', async () => {
+        const response = await request(app).delete('/api/v1/tasks/wrong-id')
+        expect(response.status).toBe(400)
+        expect(response.body).toHaveProperty('message', 'Wrong id')
+      })
+    })
   })
   afterAll(async () => {
     await request(app).get('/api/v1/seeds/flush')
-  })
-
-  describe('POST /api/v1/tasks', () => {
-    test('should return a new task created', async () => {
-      const response = await request(app).post('/api/v1/tasks').send(newTask)
-      expect(response.status).toBe(201)
-      expect(response.body).toHaveProperty('entityId')
-      for (const [key, value] of fields) {
-        expect(response.body).toHaveProperty(key, value)
-      }
-    })
-  })
-  describe('GET /api/v1/tasks', () => {
-    test('should return all tasks', async () => {
-      await request(app).post('/api/v1/tasks').send(newTask)
-
-      const response = await request(app).get('/api/v1/tasks/search')
-      const task = response.body[0]
-
-      expect(response.status).toBe(200)
-      expect(response.body).toBeInstanceOf(Array)
-      expect(task).toHaveProperty('entityId')
-      for (const [key, value] of fields) {
-        expect(task).toHaveProperty(key, value)
-      }
-    })
-  })
-
-  describe('DELETE /api/v1/tasks/:id', () => {
-    test('should return status 200 and message Task deleted', async () => {
-      const task = await request(app).post('/api/v1/tasks').send(newTask)
-      const { entityId } = task.body
-
-      const response = await request(app).delete(`/api/v1/tasks/${entityId as string}`)
-      expect(response.status).toBe(200)
-      expect(response.body).toHaveProperty('message', 'Task deleted')
-    })
-    test('should return error 400 if id is wrong', async () => {
-      const response = await request(app).delete('/api/v1/tasks/wrong-id')
-      expect(response.status).toBe(400)
-      expect(response.body).toHaveProperty('message', 'Wrong id')
-    })
   })
 })
