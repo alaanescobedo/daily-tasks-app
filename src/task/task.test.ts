4cd98442003125cd7c3f635904ea6ecc94a1cd6b
Alan Escobedo <alanescobedo.dev@gmail.com>
2022-03-27 18:55:31 -0600

    Refactor code that implements a new task interface model

--- src/task/task.test.ts
+++ src/task/task.test.ts
@@ -2,12 +2,12 @@ import request from 'supertest'
 import app from '@config/app'
 import { seedTasks } from '@seed/seed-tasks'
 
-const newTask = {
-  title: 'Test title',
-  status: 'pending',
-  scheduledFor: '2022-06-06T00:00:00.000Z'
+const TestTask = {
+  title: 'Im a title',
+  description: 'Im a description',
+  scheduledFor: '2022-03-30T00:30:35.369Z'
 }
-const fields = Object.entries(newTask)
+const fields = [...Object.keys(TestTask), 'status', 'createdAt', 'completedAt', 'updatedAt', 'entityId']
 
 describe('TASK TESTS /api/v1/tasks', () => {
   describe('GET /createindex', () => {
@@ -24,22 +24,25 @@ describe('TASK TESTS /api/v1/tasks', () => {
     })
     describe('POST /api/v1/tasks', () => {
       test('should return a new task created', async () => {
-        const response = await request(app).post('/api/v1/tasks').send(newTask)
+        const response = await request(app).post('/api/v1/tasks').send(TestTask)
         expect(response.status).toBe(201)
-        expect(response.body).toHaveProperty('entityId')
-        for (const [key, value] of fields) {
-          expect(response.body).toHaveProperty(key, value)
+        for (const field of fields) {
+          expect(response.body).toHaveProperty(field)
         }
       })
     })
     describe('GET /api/v1/tasks', () => {
       test('should return all tasks', async () => {
-        await request(app).post('/api/v1/tasks').send(newTask)
+        await request(app).post('/api/v1/tasks').send(TestTask)
 
         const response = await request(app).get('/api/v1/tasks/search')
         expect(response.status).toBe(200)
         expect(response.body).toBeInstanceOf(Array)
+        for (const field of fields) {
+          expect(response.body[0]).toHaveProperty(field)
+        }
       })
+      // TODO: Remove repeted test =>
       test('should return all the initial data', async () => {
         await request(app).get('/api/v1/seeds/generate')
         const response = await request(app).get('/api/v1/tasks/search')
@@ -50,17 +53,20 @@ describe('TASK TESTS /api/v1/tasks', () => {
 
     describe('GET /api/v1/tasks/search/:id', () => {
       test('should return status 200 and a task', async () => {
-        const { body: task } = await request(app).post('/api/v1/tasks').send(newTask)
+        const { body: task } = await request(app).post('/api/v1/tasks').send(TestTask)
 
         const response = await request(app).get(`/api/v1/tasks/search/${task.entityId as string}`)
         expect(response.status).toBe(200)
         expect(response.body).toHaveProperty('entityId', task.entityId)
+        for (const field of fields) {
+          expect(response.body).toHaveProperty(field)
+        }
       })
     })
 
     describe('DELETE /api/v1/tasks/:id', () => {
       test('should return status 200 and message Task deleted', async () => {
-        const task = await request(app).post('/api/v1/tasks').send(newTask)
+        const task = await request(app).post('/api/v1/tasks').send(TestTask)
         const { entityId } = task.body
 
         const response = await request(app).delete(`/api/v1/tasks/${entityId as string}`)
