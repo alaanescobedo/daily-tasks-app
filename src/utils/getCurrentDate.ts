0e728d1b574cbcb5427a331cc73fbdba94e09436
Alan Escobedo <alanescobedo.dev@gmail.com>
2022-04-05 18:21:03 -0500

    Add parameters in getCurrentDate

--- src/utils/getCurrentDate.ts
+++ src/utils/getCurrentDate.ts
@@ -1,12 +1,16 @@
-export const getCurrentDate = (locale: string = 'en-US'): string => {
+export const getCurrentDate = (locale: string = 'en-US', customDate?: string | number | Date, timezone?: string): string => {
+  const { timeZone } = Intl.DateTimeFormat().resolvedOptions()
+
   const date = new Intl.DateTimeFormat(locale, {
     weekday: 'long',
     day: 'numeric',
     month: 'numeric',
     year: 'numeric',
-    hour: 'numeric',
+    hour: '2-digit',
+    hourCycle: 'h24',
     minute: 'numeric',
-    second: 'numeric'
-  }).format(new Date())
+    second: 'numeric',
+    timeZone: timezone ?? timeZone
+  }).format(new Date(customDate ?? Date.now()))
   return date
 }
