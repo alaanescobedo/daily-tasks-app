01e0249fda033c6e692ce58b120c335456535404
Alan Escobedo <alanescobedo.dev@gmail.com>
2022-04-05 18:21:51 -0500

    Add layout for New Task

---
+++ src/layouts/NewTaskFormLayout.tsx
@@ -0,0 +1,21 @@
+import { AppContainerLayout } from './AppContainerLayout'
+import styles from './NewTaskFormLayout.module.css'
+
+interface Props {
+  children: React.ReactNode
+  handleSubmit: (event: React.FormEvent<HTMLFormElement>) => void
+  title?: string
+  sendLabel?: string
+}
+
+export const NewTaskFormLayout = ({ children, handleSubmit, title = 'New Task', sendLabel = 'Send' }: Props): JSX.Element => {
+  return (
+    <AppContainerLayout>
+      <h1 className={styles.heading}>{title}</h1>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        {children}
+        <button className={styles.input_submit}>{sendLabel}</button>
+      </form>
+    </AppContainerLayout>
+  )
+}
