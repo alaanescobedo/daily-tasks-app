db7a8604917ba675f0f54f7fc6fed9dacf1d568b
Alan Escobedo <alanescobedo.dev@gmail.com>
2022-04-04 19:39:02 -0500

    Add storybook to project

---
+++ .storybook/preview.js
@@ -0,0 +1,23 @@
+import { Color } from '../src/themes/config';
+
+export const parameters = {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+        matchers: {
+            color: /(background|color)$/i,
+            date: /Date$/,
+        },
+    },
+    backgrounds: {
+        default: 'dark',
+        values: [{
+                name: 'dark',
+                value: Color.DAINTREE
+            },
+            {
+                name: 'light',
+                value: Color.WHITE
+            },
+        ],
+    },
+}
