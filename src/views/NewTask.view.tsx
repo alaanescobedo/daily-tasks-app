f402b5e0d2ecc60e1474d6a0ae8e7ee6ea4f4390
Alan Escobedo <alanescobedo.dev@gmail.com>
2022-04-05 18:24:10 -0500

    Refactor the NewTask component and add it as a view

--- src/views/NewTask.view.tsx
+++ src/views/NewTask.view.tsx
@@ -1,11 +1,51 @@
-import { MainCard } from '../components'
-import { NewTask } from '../components/Card/NewTask/NewTask'
+import { Checkbox } from '../components/Form/Checkbox/Checkbox'
+import { Time } from '../components/Form/Input/TimeInput/Time'
+import { InputGroup } from '../components/Form/InputGroup/InputGroup'
+import { PriorityId, PriorityLabel, Select } from '../components/Form/Select/Select'
+import { TextArea } from '../components/Form/TextArea/TextArea'
+import { useNewTask } from '../hooks/useNewTask'
+import { NewTaskFormLayout } from '../layouts/NewTaskFormLayout'
+import styles from './NewTask.module.css'
 
-export const NewTaskView = (): JSX.Element => {
+const priorityOptions: Array<{ id: PriorityId, label: PriorityLabel }> = [
+  { id: 'low', label: 'Low' },
+  { id: 'medium', label: 'Medium' },
+  { id: 'high', label: 'High' }
+]
+
+export const NewTask = (): JSX.Element => {
+  const {
+    weekdays,
+    handleSubmit,
+    textareaValue,
+    handleTextArea,
+    selectedDay,
+    handleSelectedDay,
+    selectedHour,
+    handleSelectedHour,
+    selectedPriority,
+    handlePriority
+  } = useNewTask()
+
   return (
-    <>
-      <MainCard />
-      <NewTask />
-    </>
+    <NewTaskFormLayout sendLabel='Create Task' handleSubmit={handleSubmit}>
+      <TextArea value={textareaValue} handleChange={handleTextArea} placeholder='Task' />
+
+      <InputGroup label='Day' htmlFor='date_day' className={styles.group_date_day}>
+        <Select options={weekdays} id='date_day' handleChange={handleSelectedDay} defaultValue={selectedDay} />
+      </InputGroup>
+
+      <InputGroup label='Hour' htmlFor='date_hour' className={styles.group_date_hour}>
+        <Time id='date_hour' handleChange={handleSelectedHour} defaultValue={selectedHour} />
+      </InputGroup>
+
+      <InputGroup label='Recurrent' htmlFor='recurrent' className={styles.group_recurrent}>
+        <Checkbox id='recurrent' />
+      </InputGroup>
+
+      <InputGroup label='Priority' htmlFor='priority' className={styles.group_priority}>
+        <Select options={priorityOptions} id='priority' handleChange={handlePriority} defaultValue={selectedPriority} />
+      </InputGroup>
+    </NewTaskFormLayout>
   )
 }
