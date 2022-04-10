567ea5de1815e69798a8093cbc549258c3c86424
Alan Escobedo <alanescobedo.dev@gmail.com>
2022-04-09 20:22:29 -0500

    Add New Task inputs config

---
+++ src/config/new-task.config.ts
@@ -0,0 +1,45 @@
+import { Form_New_Task } from '../interfaces'
+
+export const NEW_TASK_INPUT_CONFIG: Form_New_Task = {
+  title: {
+    id: 'title',
+    label: 'Title',
+    type: 'text',
+    placeholder: 'Title',
+    required: true,
+    maxLength: 70,
+    value: ''
+  },
+  day: {
+    id: 'day',
+    label: 'Day',
+    type: 'date',
+    placeholder: 'Day',
+    required: true,
+    defaultValue: new Date().toISOString().split('T')[0]
+  },
+  hour: {
+    id: 'hour',
+    label: 'Hour',
+    type: 'time',
+    required: true
+    // defaultValue: getCurrentTimePlus5minutes()
+  },
+  recurrent: {
+    id: 'recurrent',
+    label: 'Recurrent',
+    type: 'checkbox',
+    defaultValue: false
+  },
+  priority: {
+    id: 'priority',
+    label: 'Priority',
+    type: 'select',
+    defaultValue: 'medium',
+    options: [
+      { id: 'low', label: 'Low' },
+      { id: 'medium', label: 'Medium' },
+      { id: 'high', label: 'High' }
+    ]
+  }
+}
