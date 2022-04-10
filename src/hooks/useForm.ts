098be8573ce1e9c54ef805d043502a8002e357fb
Alan Escobedo <alanescobedo.dev@gmail.com>
2022-04-09 22:14:19 -0500

    Add handleFieldsChange in useForm

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -1,16 +1,38 @@
-import { useState } from 'react'
+import { ChangeEvent, useState } from 'react'
+import { Form_New_Task, Input_Fields_New_Task, Input_Types_New_Task } from '../interfaces'
+import { Entries } from '../views/NewTask.view'
 
-const buildFields = (ids: any): any => {
-  return ids.reduce((acc, field) => {
-    acc[field] = { valid: false, value: '', errorMessage: '' }
+const buildFields = (fieldsConfig: Entries<Form_New_Task>): Input_Fields_New_Task => {
+  return fieldsConfig.reduce<any>((acc, field) => {
+    const [id, config] = field
+
+    acc[id] = {
+      config: { ...config },
+      isValid: false,
+      errorMessage: ''
+    }
     return acc
   }, {})
 }
 
-export const useForm = ({ ids }: any): any => {
-  const [fields] = useState(buildFields(ids))
+interface UseForm {
+  fields: Input_Fields_New_Task
+  handleFieldsChange: (e: ChangeEvent<Input_Types_New_Task>) => void
+}
 
+export const useForm = (fieldsConfig: Entries<Form_New_Task>): UseForm => {
+  const [fields, setFields] = useState(buildFields(fieldsConfig))
+
+  const handleFieldsChange = (e: ChangeEvent<Input_Types_New_Task>): any => {
+    const { id, value } = e.target as { id: keyof Form_New_Task, value: string }
+
+    setFields(() => ({
+      ...fields,
+      [id]: { ...fields[id], value }
+    }))
+  }
   return {
-    fields
+    fields,
+    handleFieldsChange
   }
 }
