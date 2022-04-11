530888d2f8389467058283f2a91fa5a21bc56e35
Alan Escobedo <alanescobedo.dev@gmail.com>
2022-04-11 14:39:06 -0500

    Add memoryRouter decorator in storybook

--- .storybook/decorators.jsx
+++ .storybook/decorators.jsx
@@ -1,4 +1,6 @@
 import { addDecorator } from '@storybook/react';
+import { MemoryRouter } from 'react-router-dom';
 import { ThemeProvider } from '../src/context/Theme/ThemeProvider';
 
-addDecorator((story) => < ThemeProvider> {story()} </ThemeProvider>)
+addDecorator((story) => <ThemeProvider> {story()} </ThemeProvider>)
+addDecorator((story) => <MemoryRouter initialEntries={['/']}> {story()} </MemoryRouter>)
