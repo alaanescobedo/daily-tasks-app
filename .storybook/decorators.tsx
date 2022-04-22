1ce2b47e9de1993ba24cf74ecea69999aa5ff5eb
Alan Escobedo <alanescobedo.dev@gmail.com>
2022-04-22 15:59:47 -0500

    Remove global styles from decorators storybook

--- .storybook/decorators.tsx
+++ .storybook/decorators.tsx
@@ -3,9 +3,6 @@ import { MemoryRouter } from 'react-router-dom';
 import { ThemeProvider } from '../src/context/Theme/ThemeProvider';
 import { TaskProvider } from '../src/context/Task/Task.provider';
 
-import styles from '../src/App.module.css';
-
-addDecorator((story) => <span className={styles.app_container}> {story()} </span>)
 addDecorator((story) => <ThemeProvider> {story()} </ThemeProvider>)
 addDecorator((story) => <TaskProvider > {story()} </TaskProvider>)
 addDecorator((story) => <MemoryRouter initialEntries={['/', '/activities/2022-04-13']}> {story()} </MemoryRouter>)
