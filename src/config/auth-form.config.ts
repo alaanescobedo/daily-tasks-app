b76473393a3e66f00fb7d02bd320228186898746
Alan Escobedo <alanescobedo.dev@gmail.com>
2022-04-26 01:57:55 -0500

    Update SignupView using yup and react hook forms

--- src/config/auth-form.config.ts
+++ src/config/auth-form.config.ts
@@ -1,49 +1,55 @@
-import { Form_Auth_Signin, Form_Auth_Signup, Form_Create_Username, Form_Forgot_Password, Form_Reset_Password } from '@interfaces'
-
 const emailConfig = {
   type: 'email',
   label: 'Email',
+  placeholder: 'user@example.com',
   id: 'email',
   required: true,
+  defaultValue: '',
   handleChange: () => { }
-}
+} as const
 const passwordConfig = {
   id: 'password',
   label: 'Password',
+  placeholder: 'Password123!',
   type: 'password',
   required: true,
+  defaultValue: '',
   handleChange: () => { }
-}
+} as const
 const passwordConfirmConfig = {
   id: 'passwordConfirm',
   label: 'Confirm Password',
+  placeholder: 'Password123!',
   type: 'password',
   required: true,
+  defaultValue: '',
   handleChange: () => { }
-}
+} as const
 const usernameConfig = {
   id: 'username',
   label: 'Username',
+  placeholder: 'Username',
   type: 'text',
   required: true,
+  defaultValue: '',
   handleChange: () => { }
-}
+} as const
 
-export const SIGNUP_INPUT_CONFIG: Form_Auth_Signup = {
+export const SIGNUP_INPUT_CONFIG = {
   email: emailConfig,
   password: passwordConfig,
   passwordConfirm: passwordConfirmConfig
 }
-export const CREATE_USER_INPUT_CONFIG: Form_Create_Username = {
+export const CREATE_USER_INPUT_CONFIG = {
   username: usernameConfig
 }
-export const SIGNIN_USER_INPUT_CONFIG: Form_Auth_Signin = {
+export const SIGNIN_USER_INPUT_CONFIG = {
   email: emailConfig,
   password: passwordConfig
 }
-export const FORGOT_PASSWORD_INPUT_CONFIG: Form_Forgot_Password = {
+export const FORGOT_PASSWORD_INPUT_CONFIG = {
   email: emailConfig
 }
-export const RESET_PASSWORD_INPUT_CONFIG: Form_Reset_Password = {
+export const RESET_PASSWORD_INPUT_CONFIG = {
   password: passwordConfig
 }
