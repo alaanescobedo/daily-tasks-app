27a0e585b60084138d0c408a661f12b6796a201a
Alan Escobedo <alanescobedo.dev@gmail.com>
2022-04-28 23:07:02 -0500

    Fix new line bugs in resizeTextarea

--- src/utils/Form/resizeTextArea.ts
+++ src/utils/Form/resizeTextArea.ts
@@ -1,10 +1,12 @@
 import { ChangeEvent } from 'react'
 
-export const resizeTextArea = (e: ChangeEvent<HTMLTextAreaElement>): void => {
+interface resizeTextareaProps {
+  e: ChangeEvent<HTMLTextAreaElement>
+}
+export const resizeTextArea = ({ e }: resizeTextareaProps): void => {
   const textarea = e.target
-  if (textarea.value.includes('\n')) return
-
-  textarea.style.height = '2.6rem'
+  if (textarea.value.includes('\n')) return // We need this to avoid new lines bugs
+  textarea.style.height = '2.6rem' // Is the same value as the height in the css
   if (textarea.scrollHeight < textarea.offsetHeight) return
 
   const scrollHeight = textarea.scrollHeight
