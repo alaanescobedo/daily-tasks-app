4e9f6ab1333d65d72c09b0805f59321967d26442
Alan Escobedo <alanescobedo.dev@gmail.com>
2022-05-03 23:33:12 -0500

    Changes in Scroll component Add fullHeight property to grow the element with "flex: 1 "

--- src/components/Scroll/Scroll.tsx
+++ src/components/Scroll/Scroll.tsx
@@ -1,12 +1,19 @@
 import styles from './Scroll.module.css'
+import cx from 'classnames'
 
 export interface ScrollProps {
   children: React.ReactNode
+  fullHeight?: boolean
 }
 
-export const Scroll = ({ children }: ScrollProps): JSX.Element => {
+export const Scroll = ({ children, fullHeight }: ScrollProps): JSX.Element => {
+  const customClassName = cx(
+    styles.scroll_container,
+    fullHeight === true && styles.full_height
+  )
+
   return (
-    <div className={styles.scroll_container}>
+    <div className={customClassName}>
       {children}
     </div>
   )
