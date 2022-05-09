869ff77543e3a8fac0293da71041092a59a2f756
Alan Escobedo <alanescobedo.dev@gmail.com>
2022-05-08 20:59:30 -0500

    Configure monorepo to work with typescript

--- packages/client/app/web/vite.config.ts
+++ packages/client/app/web/vite.config.ts
@@ -1,8 +1,17 @@
+import path from 'path'
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
-
+import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()]
+  plugins: [react(),tsconfigPaths()],
+  resolve: {
+    alias: [
+      {find:'@modules/*',replacement:path.resolve(__dirname, '/src/modules')},
+      {find:'@setup/*',replacement:path.resolve(__dirname, '/src/setup')},
+      {find:'@ui/*',replacement:path.resolve(__dirname, '/src/ui')},
+      {find:'@components/*',replacement:path.resolve(__dirname, '/src/components')},
+      {find:'@utils/*',replacement:path.resolve(__dirname, '/src/utils')},
+    ]
+  }
 })
